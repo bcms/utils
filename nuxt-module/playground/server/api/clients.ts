09d1feea15c7b3ac269cc06734a730181c54d06e
bbanez <banezprod@gmail.com>
2025-07-15 15:46:09 +0200

    move to github.com/bcms/nuxt-module

--- nuxt-module/playground/server/api/clients.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (_event) => {
-    const client = useBcmsPrivate();
-    const entries = await client.entry.getAll('blog');
-    return entries[0];
-});
--- nuxt-module/src/runtime/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Client } from '@thebcms/client';
-
-export function useBcmsPrivate(): Client {
-    return globalThis.bcmsPrivateClient;
-}
-
-export function useBcmsPublic(): Client {
-    return globalThis.bcmsPublicClient;
-}
