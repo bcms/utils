33e64c9d1d9bb72f20dc69d59fd92a5aae92ef84
bbanez <banezprod@gmail.com>
2026-02-25 16:02:18 +0100

    update packages and eslint

---
+++ components/vue/eslint.config.cjs
@@ -0,0 +1,37 @@
+import tseslint from 'typescript-eslint';
+
+export default [
+    ...tseslint.configs.recommended,
+    {
+        files: [
+            'src/**/*.{js,mjs,cjs,ts,tsx}',
+            'test/**/*.{js,mjs,cjs,ts,tsx}',
+        ],
+        rules: {
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            'no-shadow': 'error',
+            'no-unused-labels': 'warn',
+            'no-unused-expressions': 'warn',
+            'no-duplicate-imports': 'off',
+            'no-console': 'off',
+            // 'no-console': ['error', { allow: ['warn', 'error', 'debug'] }],
+            'no-debugger': 'warn',
+            '@typescript-eslint/no-shadow': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                2,
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrors: 'none',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
+    {
+        ignores: ['./node_modules/*', './dist/*'],
+    },
+];
